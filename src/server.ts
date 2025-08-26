cb146890fcda90428480e7d53272307be5f21653
Alexey Danilchik <butuzz74@gmail.com>
2025-08-26 21:13:59 +0300

    fixed server.ts

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ import productHistoryRoutes from "./routes/productHistory.routes";
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/upload", uploadRoutes);
